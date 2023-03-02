2a13a90e690a7995728c3e15de796bd2ebf8de67
cellbesmanos <cell.besmanos0119@gmail.com>
2023-03-02 08:49:04 +0800

    Feat: Configure CORS options

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://netzwelt-practical.netlify.app/Account/Login"],
+    methods: ["GET", "POST"],
+    credentials: true,
+    preflightContinue: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
 const PORT = process.env.PORT || 8080;
 
