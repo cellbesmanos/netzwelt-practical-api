b0fae482ff62e4c4b82ecbce69768856cdb52ac4
cellbesmanos <cell.besmanos0119@gmail.com>
2023-03-02 09:07:33 +0800

    Chore: Remove CORS options temporarily
    - Check if client issue was caused by CORS

--- src/index.js
+++ src/index.js
@@ -7,14 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: ["https://netzwelt-practical.netlify.app"],
-    methods: ["GET", "POST"],
-    credentials: true,
-    optionsSuccessStatus: 200,
-  })
-);
+app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 
